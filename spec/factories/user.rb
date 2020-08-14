7c7c8751c1669b1c17070e98ba139c02a6bf83d9
yuka-abe <68575380+yuka-abe@users.noreply.github.com>
2020-08-14 13:46:16 +0900

    テストコードの修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,15 +1,13 @@
-
 FactoryBot.define do
- 
   factory :user do
-    nickname              { "yamada" }
+    nickname              { 'yamada' }
     email                 { Faker::Internet.email }
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    first_name            { "ぜんかく" }
-    last_name             { "ぜんかく" }
-    first_name_kana            { "ゼンカク" }
-    last_name_kana             { "ゼンカク" }
-    birthday { "2020-01-01" }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name            { 'ぜんかく' }
+    last_name             { 'ぜんかく' }
+    first_name_kana            { 'ゼンカク' }
+    last_name_kana             { 'ゼンカク' }
+    birthday { '2020-01-01' }
   end
 end
